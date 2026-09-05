4c53e1a85f66e02125e99a98f68eef24fd5c2c84
ycbx-cgx <2698028673@qq.com>
2026-09-05 23:49:07 +0800

    type alias | mapper registry

--- src/main/java/com/ybx/mybatis/entity/Emp.java
+++ src/main/java/com/ybx/mybatis/entity/Emp.java
@@ -3,6 +3,7 @@ package com.ybx.mybatis.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.ibatis.type.Alias;
 
 @AllArgsConstructor
 @NoArgsConstructor
